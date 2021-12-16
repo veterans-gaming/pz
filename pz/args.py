ed210866f89e51dde8170f4acaab48123dbf6d5f
Mark <824103+FlyingScotsman@users.noreply.github.com>
2021-12-16 03:06:13 +0000

    Rework arguments to support sub arguments.

--- pz/args.py
+++ pz/args.py
@@ -3,5 +3,22 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Manage Project Zomboid whitelist')
-    parser.add_argument("--list-users", dest="get_users", nargs="*")
+    subparsers = parser.add_subparsers(title="Commands", dest="cmd")
+
+    list_parser = subparsers.add_parser("list", help="List users in the whitelist")
+    list_parser.add_argument("--users", nargs="*", default=[], dest="users")
+    list_parser.add_argument("--show-password", action="store_true", default=False, dest="show_password")
+
+    adduser_parser = subparsers.add_parser("adduser", help="Add users to the whitelist")
+    adduser_parser.add_argument("username", help="Player username")
+    adduser_parser.add_argument("--password", dest="password", type=str)
+    adduser_parser.add_argument("--accesslevel", choices=["player", "moderator", "admin"])
+
+    deluser_parser = subparsers.add_parser("deluser", help="Delete users from the whitelist")
+    deluser_parser.add_argument("uid", help="Player UID")
+
+    #parser.add_argument("list", nargs="?", default=[])
+    #parser.add_argument("adduser", nargs="?")
+    #parser.add_argument("--password", type=str)
+    #parser.add_argument("--accesslevel", type=str)
     return parser.parse_args()
--- pz/pz.py
+++ pz/pz.py
@@ -4,19 +4,39 @@ from pprint import pprint
 import args
 
 
+def display(headers: list, rows: list, display_password=False):
+    t = PrettyTable(headers)
+    t.add_rows(rows)
+    if not display_password:
+        t.del_column("password")
+    print(t)
+
+
+def get_users(whitelist_table, users: list):
+    if users:
+        rows = whitelist_table.get_users(users)
+    else:
+        rows = whitelist_table.get_users()
+    return rows
+
+
 def main(arguments):
     whitelist_table = database.WhitelistTable("../servertest.db")
-    whitelist_columns = whitelist_table.get_table_headers()
-    t = PrettyTable(whitelist_columns)
-    if not arguments.get_users:
-        for row in whitelist_table.get_users():
-            t.add_row(row)
-    else:
-        for row in whitelist_table.get_users(arguments.get_users):
-            t.add_row(row)
-    # t._max_width = {"password": 6}
-    t.del_column("password")
-    print(t)
+
+    if arguments.cmd == "list":
+        whitelist_columns = whitelist_table.get_table_headers()
+        rows = get_users(whitelist_table, arguments.users)
+        display(whitelist_columns, rows, arguments.show_password)
+    elif arguments.cmd == "adduser":
+        arg_list = [arguments.username]
+        if arguments.password:
+            arg_list.append(arguments.password)
+        if arguments.accesslevel:
+            arg_list.append(arguments.accesslevel)
+        whitelist_table.add_user(*arg_list)
+    elif arguments.cmd == "deluser":
+        whitelist_table.delete_user(uid=int(arguments.uid))
+
 
 
 if __name__ == '__main__':
